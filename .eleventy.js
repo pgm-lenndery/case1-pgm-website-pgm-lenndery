a89d8a12f0647b274ea7048c5e1bcb3929f91540
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-05-18 14:17:26 +0200

    Update

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,26 @@
+const fs = require("fs");
+
 module.exports = (eleventyConfig) => { 
+    
     eleventyConfig.setQuietMode(true);
     eleventyConfig.addWatchTarget("./src/");
+    
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+          ready: (err, bs) => {
+    
+            bs.addMiddleware("*", (req, res) => {
+              const content_404 = fs.readFileSync('_pages/404.html');
+              // Provides the 404 content without redirect.
+              res.write(content_404);
+              // Add 404 http status code in request header.
+              res.writeHead(404, { "Content-Type": "text/html" });
+            //   res.writeHead(404);
+              res.end();
+            });
+          }
+        }
+      });
     
     // Copy `img/` to `_site/img`
     eleventyConfig.addPassthroughCopy({ 
